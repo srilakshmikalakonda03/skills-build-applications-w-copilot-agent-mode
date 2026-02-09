430516daa3c41a2d77443eb18d1a3639d26f5ccd
srilakshmikalakonda03 <srilakshmi.kalakonda@accenture.com>
2026-02-09 11:30:12 +0000

    Step 3 changes

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from django.utils import timezone
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Clear existing data
+        Leaderboard.objects.all().delete()
+        Activity.objects.all().delete()
+        Workout.objects.all().delete()
+        User.objects.all().delete()
+        Team.objects.all().delete()
+
+        # Create Teams
+        marvel = Team.objects.create(name='Marvel', description='Team Marvel Superheroes')
+        dc = Team.objects.create(name='DC', description='Team DC Superheroes')
+
+        # Create Users
+        users = [
+            User.objects.create(name='Spider-Man', email='spiderman@marvel.com', team=marvel, is_superhero=True),
+            User.objects.create(name='Iron Man', email='ironman@marvel.com', team=marvel, is_superhero=True),
+            User.objects.create(name='Wonder Woman', email='wonderwoman@dc.com', team=dc, is_superhero=True),
+            User.objects.create(name='Batman', email='batman@dc.com', team=dc, is_superhero=True),
+        ]
+
+        # Create Activities
+        Activity.objects.create(user=users[0], type='Running', duration=30, date=timezone.now().date())
+        Activity.objects.create(user=users[1], type='Cycling', duration=45, date=timezone.now().date())
+        Activity.objects.create(user=users[2], type='Swimming', duration=60, date=timezone.now().date())
+        Activity.objects.create(user=users[3], type='Yoga', duration=40, date=timezone.now().date())
+
+        # Create Workouts
+        workout1 = Workout.objects.create(name='Super Strength', description='Strength training for superheroes')
+        workout2 = Workout.objects.create(name='Agility Training', description='Agility and speed drills')
+        workout1.suggested_for.set(users)
+        workout2.suggested_for.set(users)
+
+        # Create Leaderboard
+        Leaderboard.objects.create(user=users[0], score=100)
+        Leaderboard.objects.create(user=users[1], score=90)
+        Leaderboard.objects.create(user=users[2], score=95)
+        Leaderboard.objects.create(user=users[3], score=85)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data!'))
